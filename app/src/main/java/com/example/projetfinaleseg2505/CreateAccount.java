b6dab22f21d4f2944fd5661ba504865792fa3119
Sean Stilwell <sstil051@uottawa.ca>
2019-10-20 19:14:10 -0400

    Validating CreateAccount data - has error

--- app/src/main/java/com/example/projetfinaleseg2505/CreateAccount.java
+++ app/src/main/java/com/example/projetfinaleseg2505/CreateAccount.java
@@ -1,7 +1,7 @@
 package com.example.projetfinaleseg2505;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.EditText;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,13 +16,29 @@ public class CreateAccount extends AppCompatActivity {
 
     /** Called when the user clicks the Send button */
     public void inscrireClick(View view) {
-        // boolean variable for validating the input
-        //if variable is true {
+        // Getting the user data
+        EditText prenomText = (EditText)findViewById(R.id.textUserInput);
+        String prenom = prenomText.getText().toString();
+
+        EditText surnameText = (EditText)findViewById(R.id.textUserInput2);
+        String surname = surnameText.getText().toString();
+
+        EditText emailText = (EditText)findViewById(R.id.textUserInput3);
+        String email = emailText.getText().toString();
+
+        // Validating the user data
+        boolean validEmail = Utility.isValidEmail(email);
+        boolean validSurname = Utility.isValidName(surname);
+        boolean validPrenom = Utility.isValidName(prenom);
+
+        // Proceeding with the user data in consideration.
+        if (validEmail && validSurname && validPrenom) {
             // add data to the database
             startActivity(new Intent(CreateAccount.this, LogInScreen.class));
-        //}
-        //else {
-            // display an error where validation fails
-        //}
+        }
+        else {
+            EditText text = (EditText)findViewById(R.id.textError);
+            text.setVisibility(View.VISIBLE);
+        }
     }
 }
