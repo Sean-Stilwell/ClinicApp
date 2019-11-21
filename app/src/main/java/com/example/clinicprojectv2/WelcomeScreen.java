4c0228c9145d105ab151e4acb1535ab7f70a1e79
Sean Stilwell <sstil051@uottawa.ca>
2019-11-21 18:53:44 -0500

    Adding new activity for past/current bookings

--- app/src/main/java/com/example/clinicprojectv2/WelcomeScreen.java
+++ app/src/main/java/com/example/clinicprojectv2/WelcomeScreen.java
@@ -2,6 +2,7 @@ package com.example.clinicprojectv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -43,7 +44,10 @@ public class WelcomeScreen extends AppCompatActivity {
     public void startEmployee(View view){
 
     }
-    public void startPatient(View view){
+    public void openSearch(View view){
 
+    }
+    public void viewBookings(View view){
+        startActivity(new Intent(WelcomeScreen.this, BookingsScreen.class));
     }
 }
