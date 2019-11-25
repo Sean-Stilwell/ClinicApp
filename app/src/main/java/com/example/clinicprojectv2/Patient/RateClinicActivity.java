da0b57778a40bcb78063efc1e7f0e3565d191b4e
Enotho <mfren022@uottawa.ca>
2019-11-25 15:59:57 -0500

    Added UI for RateClinicActivity

---
+++ app/src/main/java/com/example/clinicprojectv2/Patient/RateClinicActivity.java
@@ -0,0 +1,16 @@
+package com.example.clinicprojectv2.Patient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.clinicprojectv2.R;
+
+public class RateClinicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_clinic);
+    }
+}
