483466f0dddd9e985fbdf9fbd4efe6665719e579
Matthieu Frenette <mfren022@uottawa.ca>
2019-12-03 16:58:34 -0500

    RateClinicActivity events

--- app/src/main/java/com/example/clinicprojectv2/Patient/RateClinicActivity.java
+++ app/src/main/java/com/example/clinicprojectv2/Patient/RateClinicActivity.java
@@ -3,14 +3,34 @@ package com.example.clinicprojectv2.Patient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.clinicprojectv2.Clinic.Clinic;
 import com.example.clinicprojectv2.R;
 
 public class RateClinicActivity extends AppCompatActivity {
+
+    Clinic clinic;
+    ClinicRating rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate_clinic);
+
+        // TODO Initialize rating activity using clinic
+
+        clinic = new Clinic();
+        TextView clinicNameLabel = (TextView)findViewById(R.id.clinicName);
+        clinicNameLabel.setText(clinic.getName());
+    }
+
+    public void createRating(View view) {
+        // TODO create rating
+    }
+
+    public void cancelRating(View view) {
+        this.finish();
     }
 }
